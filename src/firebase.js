77a1fe1e49057a8a73685d0cb9c27523f11e4dbe
Johann Gomez <johanngomez02@gmail.com>
2022-10-16 12:31:03 -0500

    changes in connection for firestore

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp }  from "firebase/app";
-import "firebase/firestore"
+import { getFirestore } from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,4 +15,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = app.firestore();
+export const db = getFirestore(app);
